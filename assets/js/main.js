8a3c25e461db95fecc8840619c9911168a05dc4b
actraStride <actradev@protonmail.com>
2024-08-04 19:17:41 -0600

    Cambios menores 04/08/24

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
 
 window.onload = function(){
     let slideshow = document.getElementById("slideContainer")
-    let slideshowImages = ["slideOne.jpg", "slideTwo.jpg", "slideThree.jpg", "slideFour.jpg"]
+    let slideshowImages = ["slideOne.jpg", "slideTwo.jpg", "slideThree.jpg"]
     let slidePosition = 0
 
     let intervalChange = setInterval(function(){
-        if(slidePosition == slideshowImages.length - 1){
+        if(slidePosition == slideshowImages.length ){
             slidePosition = 0
         }
-        slidePosition += 1 
         slideshow.style.backgroundImage = "url(../../assets/img/" + slideshowImages[slidePosition] + ")"
+        slidePosition += 1 
     }, 3000)
 
-    
+    window.addEventListener("scroll",function(){
+        let header = document.getElementById("header")
+        let scrollPosition = window.scrollY
+
+        if(scrollPosition > 40){
+            header.style.backgroundColor = "blue"
+        }
+        else{
+            header.style.backgroundColor = "white"
+        }
+       
+    })
 
 }
